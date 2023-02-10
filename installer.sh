699d70d785ad1447e1c706989265ebeb5452c411
pengshiyu <1940607002@qq.com>
2023-02-10 17:33:15 +0800

    fix

--- installer.sh
+++ installer.sh
@@ -4,9 +4,11 @@
 # quick-env 一键安装器
 ##################################
 
+# wget https://gitcode.net/mouday/quick-env/-/archive/${version}/quick-env-${version}.tar.gz && \
+
 export version=0.0.3 && \
 yum install -y wget && \
-wget https://gitcode.net/mouday/quick-env/-/archive/${version}/quick-env-${version}.tar.gz && \
+wget https://github.com/mouday/quick-env/archive/refs/tags/${version}.tar.gz -O quick-env-${version}.tar.gz && \
 tar -zxf quick-env-${version}.tar.gz && \
 cd quick-env-${version} && \
 bash install.sh
