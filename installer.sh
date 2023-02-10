42149bb814979119b50f2167b9f6c6f873d00372
pengshiyu <1940607002@qq.com>
2023-02-10 22:56:42 +0800

    publish 0.0.5

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@
 # wget https://gitcode.net/mouday/quick-env/-/archive/${version}/quick-env-${version}.tar.gz && \
 # wget https://github.com/mouday/quick-env/archive/refs/tags/${version}.tar.gz -O quick-env-${version}.tar.gz
 
-export version=0.0.4 && \
+export version=0.0.5 && \
 yum install -y wget && \
 wget https://ghproxy.com/https://github.com/mouday/quick-env/archive/refs/tags/${version}.tar.gz -O quick-env-${version}.tar.gz && \
 tar -zxf quick-env-${version}.tar.gz && \
